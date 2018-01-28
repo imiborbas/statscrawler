c42516f190b56ab82d45f3af596392d082445b4f
Imi Borbas <imiborbas@gmail.com>
2018-01-28 19:16:13 +0000

    Load subdirectories under /lib automatically

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Buzzcrawl
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Enable subfolder autoloading in lib/
+    config.autoload_paths += Dir[Rails.root.join('lib', '{*/}')]
   end
 end
