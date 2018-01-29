1d530cce83abb29f83bab3ace00d4dae109b5973
Imi Borbas <imiborbas@gmail.com>
2018-01-29 13:52:34 +0000

    Set up database in the test runner

--- test/run_test.rb
+++ test/run_test.rb
@@ -1,5 +1,8 @@
 require 'bundler/setup'
 require 'test/unit'
+require 'active_record'
+
+### Take care of autoloading all the dependencies needed to run the tests ###
 
 Bundler.require(:default, :test)
 
@@ -13,12 +16,26 @@ def require_paths(base_dir, paths)
 end
 
 require_paths(base_dir, %w[
+  app/models/*.rb
   lib/**/*.rb
 ])
+
+### Configure VCR to use HTTP fixtures so that we won't use network during testing ###
 
 VCR.configure do |config|
   config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
 end
+
+### Set up database connection ###
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: 'db/test.sqlite3'
+)
+
+ActiveRecord::Migrator.migrate('db/migrate/')
+
+### Execute test files ###
 
 exit Test::Unit::AutoRunner.run(true, test_dir)
